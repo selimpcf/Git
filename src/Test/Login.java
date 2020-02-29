b6938b7475caa20c5fca66825dd438f193beacca
selimpcf <58636568+selimpcf@users.noreply.github.com>
2020-02-29 11:33:59 -0600

    update Logout.java

--- src/Test/Login.java
+++ src/Test/Login.java
@@ -3,5 +3,6 @@ package Test;
 public class Login {
     public static void main(String[] args) {
         System.out.println("hello world selim!");
+        System.out.println("yes modified too");
     }
 }
--- src/Test/Logout.java
+++ src/Test/Logout.java
@@ -3,6 +3,6 @@ import java.security.spec.RSAOtherPrimeInfo;
 public class Logout {
     public static void main(String[] args) {
         System.out.println("123");
-
+        System.out.println("modified again");
     }
 }
